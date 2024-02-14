f17a706d52336113911da26dca368101c04b4d48
CintiaSilva7300 <88053580+CintiaSilva7300@users.noreply.github.com>
2024-02-14 13:13:08 -0300

    first commit

---
+++ src/paginas/home/Home.jsx
@@ -0,0 +1,16 @@
+import "./style.css";
+
+function Home() {
+  return (
+    <>
+      <h1 className="title">Home</h1>
+      <img
+        className="img"
+        src="https://img.freepik.com/fotos-gratis/uma-pintura-de-um-lago-de-montanha-com-uma-montanha-ao-fundo_188544-9126.jpg"
+        alt=""
+      />
+    </>
+  );
+}
+
+export default Home;
